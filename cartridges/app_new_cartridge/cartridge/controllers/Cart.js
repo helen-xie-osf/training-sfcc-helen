62aa8ef28b217460c6c22dfb8d08f124a34b466a
helen-xie-osf <helen.xie@osf.digital>
2022-08-31 12:29:00 +0900

    source reorganization, check product set

--- cartridges/app_new_cartridge/cartridge/controllers/Cart.js
+++ cartridges/app_new_cartridge/cartridge/controllers/Cart.js
@@ -9,6 +9,36 @@ var controller = require('app_storefront_base/cartridge/controllers/Cart');
 var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
 var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
 
+server.append(
+    'Show',
+    server.middleware.https,
+    consentTracking.consent,
+    csrfProtection.generateToken,
+    function (req, res, next) {
+
+    var BasketMgr = require('dw/order/BasketMgr');
+    var Transaction = require('dw/system/Transaction');
+    var currentBasket = BasketMgr.getCurrentBasket();
+    var cartHelper = require('*/cartridge/scripts/cart/cartHelpers');
+    var basketCalculationHelpers = require('*/cartridge/scripts/helpers/basketCalculationHelpers');
+
+    const TOTAL_AMOUNT_THRESHOLD = 200;
+    var totalAmountThreshold = 0;
+    var amountThreshold;
+    if(currentBasket){
+        Transaction.wrap(function () {
+            amountThreshold = currentBasket.getMerchandizeTotalNetPrice().decimalValue;
+           if (amountThreshold >= TOTAL_AMOUNT_THRESHOLD) {
+                totalAmountThreshold = TOTAL_AMOUNT_THRESHOLD;
+            }
+        });
+    }
+
+    res.render('cart/cart', { totalAmountThreshold: totalAmountThreshold});
+
+    return next();
+
+});
 
 /**
  * Cart-AddProduct : The Cart-MiniCart endpoint is responsible for displaying the cart icon in the header with the number of items in the current basket
